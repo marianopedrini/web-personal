7ad7890d1174e9b024eabd6eac7f86008ed95618
marianopedrini <pedrini.mariano96@gmail.com>
2022-02-06 20:13:31 -0300

    fixes

--- js/formValidation.js
+++ js/formValidation.js
@@ -1,12 +1,12 @@
 /* ---- FORM ---- */
-const formContact = document.getElementById("formContact");
+const formContact = document.getElementById('formContact');
 
-const inputs = document.querySelectorAll("#formContact input");
-const textarea = document.getElementById("message");
+const inputs = document.querySelectorAll('#formContact input');
+const textarea = document.getElementById('message');
 
-const errorName = document.getElementById("errorName");
-const errorEmail = document.getElementById("errorEmail");
-const errorMessage = document.getElementById("errorMessage");
+const errorName = document.getElementById('errorName');
+const errorEmail = document.getElementById('errorEmail');
+const errorMessage = document.getElementById('errorMessage');
 
 const camps = {
   name: false,
@@ -21,50 +21,50 @@ function validarEmail(email) {
 
 const validateCamp = (e) => {
   switch (e.target.name) {
-    case "name":
-      if (e.target.value == "") {
-        errorName.textContent = "Debes ingresar un nombre";
-        e.target.style.border = "1px solid red";
+    case 'name':
+      if (e.target.value == '') {
+        // errorName.textContent = "Debes ingresar un nombre";
+        // e.target.style.border = "1px solid red";
         camps.name = false;
       } else if (e.target.value.length < 3) {
-        errorName.textContent = "Ingresa un nombre válido";
-        e.target.style.border = "1px solid red";
+        errorName.textContent = 'Ingresa un nombre válido';
+        e.target.style.border = '1px solid red';
         camps.name = false;
       } else {
-        errorName.textContent = "";
-        e.target.style.border = "none";
+        errorName.textContent = '';
+        e.target.style.border = 'none';
         camps.name = true;
       }
       break;
 
-    case "email":
-      if (e.target.value == "") {
-        errorEmail.textContent = "Debes ingresar un email";
-        e.target.style.border = "1px solid red";
+    case 'email':
+      if (e.target.value == '') {
+        // errorEmail.textContent = "Debes ingresar un email";
+        // e.target.style.border = "1px solid red";
         camps.email = false;
       } else if (validarEmail(e.target.value) == false) {
-        errorEmail.textContent = "Ingresar un email válido";
-        e.target.style.border = "1px solid red";
+        errorEmail.textContent = 'Ingresar un email válido';
+        e.target.style.border = '1px solid red';
         camps.email = false;
       } else {
-        errorEmail.textContent = "";
-        e.target.style.border = "none";
+        errorEmail.textContent = '';
+        e.target.style.border = 'none';
         camps.email = true;
       }
       break;
 
-    case "message":
-      if (e.target.value == "") {
-        errorMessage.textContent = "Agrega el motivo de tu contacto";
-        e.target.style.border = "1px solid red";
+    case 'message':
+      if (e.target.value == '') {
+        // errorMessage.textContent = "Agrega el motivo de tu contacto";
+        // e.target.style.border = "1px solid red";
         camps.message = false;
       } else if (e.target.value.length < 30) {
-        errorMessage.textContent = "Por favor sea mas descriptivo";
-        e.target.style.border = "1px solid red";
+        errorMessage.textContent = 'Por favor sea mas descriptivo';
+        e.target.style.border = '1px solid red';
         camps.message = false;
       } else {
-        errorMessage.textContent = "";
-        e.target.style.border = "none";
+        errorMessage.textContent = '';
+        e.target.style.border = 'none';
         camps.message = true;
       }
       break;
@@ -72,28 +72,28 @@ const validateCamp = (e) => {
 };
 
 inputs.forEach((input) => {
-  input.addEventListener("keyup", validateCamp);
-  input.addEventListener("blur", validateCamp);
+  input.addEventListener('keyup', validateCamp);
+  input.addEventListener('blur', validateCamp);
 });
-textarea.addEventListener("keyup", validateCamp);
-textarea.addEventListener("blur", validateCamp);
+textarea.addEventListener('keyup', validateCamp);
+textarea.addEventListener('blur', validateCamp);
 
-formContact.addEventListener("submit", (e) => {
+formContact.addEventListener('submit', (e) => {
   if (!camps.name) {
-    errorName.textContent = "Debes completar con un nombre";
+    errorName.textContent = 'Debes completar con un nombre';
     e.preventDefault();
   }
   if (!camps.email) {
-    errorEmail.textContent = "Debes ingresar un email";
+    errorEmail.textContent = 'Debes ingresar un email';
     e.preventDefault();
   }
   if (!camps.message) {
-    errorMessage.textContent = "Ingresa el motivo de tu contacto";
+    errorMessage.textContent = 'Ingresa el motivo de tu contacto';
     e.preventDefault();
   }
 });
 
-formContact.addEventListener("submit", handleSubmit);
+formContact.addEventListener('submit', handleSubmit);
 
 async function handleSubmit(event) {
   event.preventDefault();
@@ -102,7 +102,7 @@ async function handleSubmit(event) {
     method: this.method,
     body: form,
     headers: {
-      Accept: "application/json",
+      Accept: 'application/json',
     },
   });
   if (response.ok) {
@@ -111,13 +111,13 @@ async function handleSubmit(event) {
   }
 }
 
-let xCloseMessage = document.querySelector(".ok-message__close");
-let message = document.querySelector(".ok-message");
+let xCloseMessage = document.querySelector('.ok-message__close');
+let message = document.querySelector('.ok-message');
 
-xCloseMessage.addEventListener("click", function () {
-  message.classList.toggle("show-message");
+xCloseMessage.addEventListener('click', function () {
+  message.classList.toggle('show-message');
 });
 
 function showMessage() {
-  message.classList.toggle("show-message");
+  message.classList.toggle('show-message');
 }
